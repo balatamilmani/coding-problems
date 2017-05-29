5acbdd45ac094c7d37913cb72a11c502fefdcd31
balatamilmani <btamilma.ptr@gmail.com>
2017-05-29 09:03:28 -0400

    Linked List adder

---
+++ list-adder/src/main/java/com/balatamilmani/listadder/LinkedListAdder.java
@@ -0,0 +1,91 @@
+package com.balatamilmani.listadder;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.ListIterator;
+
+public class LinkedListAdder {
+
+	/**
+	 * Given Two Linked List's of Integers, this method returns its sum as Linked List
+	 * For e.g. [1,2,3], [5,6] will result in [1,7,9]
+	 * @param listOne The first value to be added
+	 * @param listTwo The second value to be added
+	 * @return List contain the sum of the values
+	 * @throws Exception If both the Lists are Null 
+	 * or when one of the List is null the other is empty 
+	 * or both Lists are empty 
+	 */
+	public static List<Integer> add(List<Integer> listOne, List<Integer> listTwo) throws Exception {
+		//Validate input
+		validate(listOne, listTwo);
+
+		List<Integer> listResult = new LinkedList<Integer>();
+		List<Integer> longerList = null;
+		List<Integer> shorterList = null;
+		ListIterator<Integer> longListIt = null;
+		ListIterator<Integer> shortListIt = null;
+		
+		int firstValue, secondValue, sum=0, reminder=0, quotient=0;
+		
+		if(listOne == null || !(listOne.size()>0)){
+			//First List is null the result is just the Second List
+			listResult = new LinkedList<>(listTwo);
+		} else if(listTwo == null || !(listTwo.size()>0)) {
+			//Second List is null the result is just the First List
+			listResult = new LinkedList<>(listOne);
+		} else {
+			//Both Lists non empty
+			if(listOne.size()>listTwo.size()){
+				longerList = new LinkedList<>(listOne);
+				shorterList = new LinkedList<>(listTwo);
+			} else {
+				//Both Lists same size also second List will be considered as Long List
+				longerList = new LinkedList<>(listTwo);
+				shorterList = new LinkedList<>(listOne);
+			}
+			
+			longListIt = longerList.listIterator(longerList.size());
+			shortListIt = shorterList.listIterator(shorterList.size());
+			
+			while(longListIt.hasPrevious()) {
+				reminder = 0;
+				secondValue = 0;
+				firstValue = longListIt.previous();
+				if(shortListIt.hasPrevious()){
+					secondValue = shortListIt.previous();
+				}
+				sum = firstValue + secondValue + quotient;
+				quotient = sum/10;
+				reminder = sum%10;
+				//Reminder added to Result, quotient will be carried over to 
+				listResult.add(0,reminder);
+			}
+			if(sum>=10){
+				listResult.add(0, quotient);
+			}
+		}
+		return listResult;
+	}
+
+	/**
+	 * @param listOne List containing the Int values
+	 * @param listTwo List containing the Int values
+	 * @throws Exception If both the Lists are Null 
+	 * or when one of the List is null the other is empty 
+	 * or both Lists are empty  
+	 */
+	private static void validate(List<Integer> listOne, List<Integer> listTwo) throws Exception {
+		//Both lists can't be null
+		if(		(listOne == null && listTwo == null)	){
+			throw new Exception("Both lists can't be Null");
+		}
+		//One of the list should have values
+		if(		(listOne == null && !(listTwo.size()>0)) || 
+				(listTwo == null && !(listOne.size()>0)) ||
+				((listOne != null && listTwo != null) && !(listOne.size()>0) && !(listTwo.size()>0))
+				){
+			throw new Exception("The list(s) can't be empty. At least one list must have value");
+		}
+	}
+}
---
+++ list-adder/src/test/java/com/balatamilmani/listadder/LinkedListAdderTest.java
@@ -0,0 +1,226 @@
+/*
+ * Copyright (c) 2017. Balamurugan Tamilmani (balamurugan.leo@gmail.com). All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are not permitted.
+ */
+package com.balatamilmani.listadder;
+
+import java.util.LinkedList;
+import java.util.List;
+
+import com.balatamilmani.listadder.LinkedListAdder;
+
+import junit.framework.Assert;
+import junit.framework.TestCase;
+
+/**
+ * @author Balamurugan Tamilmani
+ * Test code for Linked List adder
+ */
+public class LinkedListAdderTest extends TestCase {
+
+
+	/**
+	 * Both the Lists are null, Expect Exception
+	 */
+	public void testAddAllNullNotAllowedThrowsException() {
+		List<Integer> listOne = null;
+		List<Integer> listTwo = null;
+		try {
+			LinkedListAdder.add(listOne, listTwo);
+		} catch (Exception e) {
+			Assert.assertTrue(e.toString().contains("Both lists can't be Null"));
+		}
+	}
+
+	/**
+	 * One List is Empty and other Null, Except Exception
+	 */
+	public void testAddOneListMustContainValueThrowsException() {
+		List<Integer> listOne = new LinkedList<>();
+		List<Integer> listTwo = null;
+		try {
+			LinkedListAdder.add(listOne, listTwo);
+		} catch (Exception e) {
+			Assert.assertTrue(e.toString().contains("The list(s) can't be empty. At least one list must have value"));
+		}
+	}
+
+	/**
+	 * Both Lists are not Null, however Empty, Expect Exception
+	 */
+	public void testAddBothNotNullListNoValuesThrowsException() {
+		List<Integer> listOne = new LinkedList<>();
+		List<Integer> listTwo = new LinkedList<>();
+		try {
+			LinkedListAdder.add(listOne, listTwo);
+		} catch (Exception e) {
+			Assert.assertTrue(e.toString().contains("The list(s) can't be empty. At least one list must have value"));
+		}
+	}
+	
+	/**
+	 * First List has values, second List is null, Expect simply First List returned
+	 * 
+	 */
+	public void testAddFirstListValuesSecondNullExpectFirstList() {
+		List<Integer> listOne = new LinkedList<>();
+		List<Integer> listTwo = null;
+		List<Integer> listResult = null;
+		listOne.add(1);
+		listOne.add(2);
+		listOne.add(3);
+		try {
+			listResult = LinkedListAdder.add(listOne, listTwo);
+			Assert.assertEquals(listOne, listResult);
+		} catch (Exception e) {
+			Assert.fail();
+		}
+	}
+
+	/**
+	 * Second List has values, First List is null, Expect simply Second List returned
+	 * 
+	 */
+	public void testAddSecondListValuesFirstNullExpectSecondList() {
+		List<Integer> listOne = null;
+		List<Integer> listTwo = new LinkedList<>();
+		List<Integer> listResult = null;
+		listTwo.add(1);
+		listTwo.add(2);
+		listTwo.add(3);
+		try {
+			listResult = LinkedListAdder.add(listOne, listTwo);
+			Assert.assertEquals(listTwo, listResult);
+		} catch (Exception e) {
+			e.printStackTrace();
+			Assert.fail();
+		}
+	}
+	
+	/**
+	 * First List has values, second List is not null but Empty, Expect simply First List returned
+	 */
+	public void testAddFirstListValuesSecondNonNullEmptyExpectFirstList() {
+		List<Integer> listOne = new LinkedList<>();
+		List<Integer> listTwo = new LinkedList<>();
+		List<Integer> listResult = null;
+		listOne.add(1);
+		listOne.add(2);
+		listOne.add(3);
+		try {
+			listResult = LinkedListAdder.add(listOne, listTwo);
+			Assert.assertEquals(listOne, listResult);
+		} catch (Exception e) {
+			Assert.fail();
+		}
+	}
+
+	/**
+	 * Second List has values, First List is not null but Empty, Expect simply Second List returned
+	 */
+	public void testAddSecondListValuesFirstNonNullEmptyExpectSecondList() {
+		List<Integer> listOne = new LinkedList<>();
+		List<Integer> listTwo = new LinkedList<>();
+		List<Integer> listResult = null;
+		listTwo.add(1);
+		listTwo.add(2);
+		listTwo.add(3);
+		try {
+			listResult = LinkedListAdder.add(listOne, listTwo);
+			Assert.assertEquals(listTwo, listResult);
+		} catch (Exception e) {
+			Assert.fail();
+		}
+	}
+	
+	/**
+	 * Both Lists are not null, expect its sum returned
+	 */
+	public void testAddBothListHasValueNoCarryOver() {
+		List<Integer> listOne = new LinkedList<Integer>();
+		List<Integer> listTwo = new LinkedList<Integer>();
+		List<Integer> listExpected = new LinkedList<Integer>();
+		List<Integer> listResult = null;
+		
+		listOne.add(1);
+		listOne.add(2);
+		listOne.add(3);
+		
+		listTwo.add(5);
+		listTwo.add(6);
+		
+		listExpected.add(1);
+		listExpected.add(7);
+		listExpected.add(9);
+		
+		try {
+			listResult = LinkedListAdder.add(listOne, listTwo);
+			Assert.assertEquals("[1, 7, 9]", listResult.toString());
+			Assert.assertEquals(listExpected, listResult);
+		} catch (Exception e) {
+			Assert.fail();
+		}
+	}
+	
+	/**
+	 * The reminder is carried over
+	 */
+	public void testAddBothListHasValueWithCarryOver() {
+		List<Integer> listOne = new LinkedList<Integer>();
+		List<Integer> listTwo = new LinkedList<Integer>();
+		List<Integer> listExpected = new LinkedList<Integer>();
+		List<Integer> listResult = null;
+		
+		listOne.add(6);
+		listOne.add(7);
+		listOne.add(8);
+		
+		listTwo.add(9);
+		listTwo.add(8);
+		
+		listExpected.add(7);
+		listExpected.add(7);
+		listExpected.add(6);
+		
+		try {
+			listResult = LinkedListAdder.add(listOne, listTwo);
+			Assert.assertEquals("[7, 7, 6]", listResult.toString());
+			Assert.assertEquals(listExpected, listResult);
+		} catch (Exception e) {
+			Assert.fail();
+		}
+	}
+	
+	/**
+	 * The reminder is carried over. Both Lists same size
+	 */
+	public void testAddBothListHasValueSameSizeWithCarryOver() {
+		List<Integer> listOne = new LinkedList<Integer>();
+		List<Integer> listTwo = new LinkedList<Integer>();
+		List<Integer> listExpected = new LinkedList<Integer>();
+		List<Integer> listResult = null;
+		
+		listOne.add(6);
+		listOne.add(7);
+		listOne.add(8);
+		
+		listTwo.add(5);
+		listTwo.add(6);
+		listTwo.add(7);
+		
+		listExpected.add(1);
+		listExpected.add(2);
+		listExpected.add(4);
+		listExpected.add(5);
+		
+		try {
+			listResult = LinkedListAdder.add(listOne, listTwo);
+			Assert.assertEquals("[1, 2, 4, 5]", listResult.toString());
+			Assert.assertEquals(listExpected, listResult);
+		} catch (Exception e) {
+			Assert.fail();
+		}
+	}
+}
