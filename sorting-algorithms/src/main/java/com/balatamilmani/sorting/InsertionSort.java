025f1785ad272f8e7acbee5f140e990b1dd49178
balatamilmani <btamilma.ptr@gmail.com>
2017-08-16 08:06:58 -0400

    Sorting algorithms

---
+++ sorting-algorithms/src/main/java/com/balatamilmani/sorting/InsertionSort.java
@@ -0,0 +1,29 @@
+package com.balatamilmani.sorting;
+
+import java.util.Arrays;
+
+public class InsertionSort {
+
+	public static void main(String[] args) {
+		int unsortedArray[]={12,8,55,3,10,93};
+		System.out.println("Before sorting");
+		System.out.println(Arrays.toString(unsortedArray));
+		System.out.println("After sorting");
+		System.out.println(Arrays.toString(sort(unsortedArray)));
+	}
+	
+	private static int[] sort(int a[]){
+		//Loop from second element upto last one
+		for(int j=1; j<a.length; j++){
+			int val = a[j]; //Everything below j is sorted
+			//compare a[j] with all elements starting from a[j-1] until a[0]
+			int i=j-1;
+			while(i>=0 && a[i]>val){
+				a[i+1] = a[i]; //keep moving the value to right until end or a smaller value is found
+				i=i-1;
+			}
+			a[i+1] = val;
+		}
+		return a;
+	}
+}
